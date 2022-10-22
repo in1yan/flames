8368fccc8b9452087d0df07be0c1f4084f0b0b1c
Iniyan <78460090+iniyan2006@users.noreply.github.com>
2022-10-22 16:54:08 +0530

    correctoins/...

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY']="ThisIsMe"
 first_name = ""
 second_name = ""
+exceptions = [
+    'iniyan',
+    'vignesh',
+    'anees'
+]
 @app.route('/', methods = ["GET", "POST"]) 
 def index():
     if request.method == "POST":
@@ -11,12 +16,13 @@ def index():
         second_name = request.form.get("sname").lower()
         if first_name == 'admin' and second_name == 'admin':
             return redirect('/admin')
-        else:
+        elif first_name not in execeptions and second_name not in exceptions:
             status = flames(first_name, second_name)
             emoji = status.split(" ")
             status = emoji[0]
             with open('./info.txt','a') as f:
                 f.write(f"\n  first name : {first_name} second_name : {second_name}  result : {status[0]}")
+                f.close()
             return render_template('result.html',status=status, emoji=emoji[1], first_name=first_name, second_name=second_name) 
     return render_template('index.html')
 @app.route('/admin')
@@ -24,5 +30,6 @@ def admin():
     file = './info.txt'
     with open(file, 'r') as f:
         file = f.readlines()
+        f.close()
     return render_template('admin.html', file = file) 
 app.run(debug = True, host = "0.0.0.0")
