cd11a29b2b5da8d6492c7174da87b2b368ff5aa8
Iniyan <viniyan563@gmail.com>
2023-02-12 11:23:17 +0530

    corrections

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, redirect
-import datetime
+from datetime import datetime
 from utils import flamer
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "ThisIsMe"
